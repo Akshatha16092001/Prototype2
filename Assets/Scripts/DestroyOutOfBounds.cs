16e7ac27edbd2185763adfca9740673f75f9cadd
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-24 15:11:03 +0530

    addition of low bound to the game

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float topBound = 30.0f;
+    private float lowerbound = -10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z > lowerbound)
+        {
+             Destroy(gameObject);
+        }
+    }
+}
