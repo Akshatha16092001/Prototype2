a0b28dd8cb995c50f5f936a86549d25688ed6efa
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-24 16:40:54 +0530

    init commit

--- Assets/Scripts/SpawmManager.cs
+++ Assets/Scripts/SpawmManager.cs
@@ -17,14 +17,19 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
 
 
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            SpawnRandomAnimal();
+        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+         int animalIndex = Random.Range(0, animalPrefabs.Length);
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
-        
     }
+
 }
