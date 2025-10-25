e238a416b411efda8f3ec81eb28823d93027d2b3
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-25 09:48:24 +0530

    Adding game over statement

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -21,6 +21,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if(transform.position.z < lowerbound)
         {
+            Debug.Log("Game over");
              Destroy(gameObject);
         }
     }
