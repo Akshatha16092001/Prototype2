3ae4bbf765a40a1bc0b7cabc01d3256a80b71274
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-25 13:28:40 +0530

    challenges

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //added code
+            Vector3 spawnPos = new Vector3(transform.position.x, 0, transform.position.z);
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
