d75d542257d84a669e552dbfbe9b2542f6db35ca
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-25 09:43:56 +0530

    Addition of other game object

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -18,7 +18,7 @@ public class DetectCollisions : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
-        //Destroy(other.gameObject);
+        Destroy(other.gameObject);
         
     }
 }
