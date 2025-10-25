18f301077a1cdf12dae2b52ac74fe86b1c6b90dc
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-25 09:49:39 +0530

    adding exclamatory symbol

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -21,7 +21,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if(transform.position.z < lowerbound)
         {
-            Debug.Log("Game over");
+            Debug.Log("Game over!");
              Destroy(gameObject);
         }
     }
